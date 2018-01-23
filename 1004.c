663afa0965b6574a9a4a2b42671b7a527415349d
linpeijie <lpj498840744@qq.com>
2018-01-23 12:24:34 +0800

    Update 1004.c

--- 1004.c
+++ 1004.c
@@ -2,18 +2,20 @@
 #include<string.h>
 
 int main(){
-    char s[1001][15];
+    char s[1001][1000];
     int n,i,j,flag;
-    int max=0,sum=0;
+    int max,sum;
 
     while(scanf("%d",&n)!=EOF && n){
         for(i=0;i<n;i++){
             scanf("%s",s[i]);
         }
-
+        
+        max=-1;
         for(i=0;i<n;i++){
+            sum=0;
             for(j=i+1;j<n;j++){
-                if((strcmp(s[i],s[j]))==0)
+                if(strcmp(s[i],s[j])==0)
                     sum++;
                 if(sum > max){
                     max = sum;
