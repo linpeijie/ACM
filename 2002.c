95e1112bec9177ef3aeb7d642b2acd97259eaf12
lpj498840744 <lpj498840744@qq.com>
2018-02-07 10:10:38 +0800

    calculate the sphere volume

---
+++ 2002.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define PI 3.1415927
+
+int main(){
+    double r,v;
+
+    while(scanf("%lf",&r)!=EOF){
+        v = (4*PI*r*r*r)/3;
+        printf("%.3lf\n",v);
+    }
+
+    return 0;
+}
