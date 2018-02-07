b1fb768da55e470cb971fdadf6c06114d5903596
lpj498840744 <lpj498840744@qq.com>
2018-02-07 22:27:43 +0800

    summation

--- 2011.c
+++ 2011.c
@@ -1,21 +1,28 @@
 #include<stdio.h>
 
 int main(){
-    int m,n[1000];
-    int i,j;
+    int n[1000];
+    int m,i,j;
     double result;
 
     scanf("%d",&m);
-    for(i=0;i<m;i++)
-        scanf("%d",n[i]);
+    for(i=0;i<m;i++){
+        scanf("%d",&j);
+        n[i] = j;
+    }
 
     for(i=0;i<m;i++){
 
         result=0;
 
         for(j=1;j<=n[i];j++){
-                        
+            if(j%2!=0)
+                result += 1.0/j;
+            else
+                result -= 1.0/j;            
         }
+
+        printf("%.2lf\n",result);
     }
     
     return 0;
