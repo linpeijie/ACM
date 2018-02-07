aeccb8f59d4fdc64f82af8804abd65a82eff2e1f
lpj498840744 <lpj498840744@qq.com>
2018-02-07 10:39:13 +0800

    summation

---
+++ 2007.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int m,n;
+    int x,y;
+    int i;
+    
+    while(scanf("%d%d",&m,&n)!=EOF){
+        x=y=0;
+
+        for(i=m;i<=n;i++){
+            if(i%2==0)
+                x += i*i;
+            else
+                y += i*i*i;
+        }
+
+        printf("%d %d\n",x,y);
+
+    }
+
+    return 0;
+}
