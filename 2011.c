15dc3e09a2c218acdde1f6b10e58fd9e6cf4b8c2
lpj498840744 <lpj498840744@qq.com>
2018-02-07 21:24:39 +0800

    summation

---
+++ 2011.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int m,n[1000];
+    int i,j;
+    double result;
+
+    scanf("%d",&m);
+    for(i=0;i<m;i++)
+        scanf("%d",n[i]);
+
+    for(i=0;i<m;i++){
+
+        result=0;
+
+        for(j=1;j<=n[i];j++){
+                        
+        }
+    }
+    
+    return 0;
+}
