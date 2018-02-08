228f27f0705d440fa63d3ed03fe218bae2a98600
lpj498840744 <lpj498840744@qq.com>
2018-02-08 21:54:50 +0800

    find minimen number

--- 2016.c
+++ 2016.c
@@ -1,29 +1,32 @@
 #include<stdio.h>
 
 int main(){
-    int n[101],m;
-    int i,min,flag,index;
+    int n[101];
+    int i,m,min,flag,index;
 
     while(scanf("%d",&m)!=EOF && m!=0){
-        min = 0;
+
+        for(i=0;i<m;i++)
+            scanf("%d",&n[i]);
+
+        min = n[0];
+        flag = 0;
 
         for(i=0;i<m;i++){
-            scanf("%d",&n[i]);
             if(n[i]<min){
                 min = n[i];
                 flag = i;
             }
         }
 
-        if(flag!=0){
-            index = n[0];
-            n[0] = n[flag];
-            n[flag] = index;
-        }
+        index = n[0];
+        n[0] = n[flag];
+        n[flag] = index;
 
-        for(i=0;i<m;i++)
+
+        for(i=0;i<m-1;i++)
             printf("%d ",n[i]);
-        printf("\n");
+        printf("%d\n",n[m-1]);
 
     }
 
