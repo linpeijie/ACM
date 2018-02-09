329d0fc5e1d88d3fb642f368cc982a3d9a0f2d96
lpj498840744 <lpj498840744@qq.com>
2018-02-09 20:14:38 +0800

    string and array

--- 2025.c
+++ 2025.c
@@ -2,8 +2,25 @@
 #include<string.h>
 
 int main(){
-    char ch[1000];
+    char ch[101],max;
+    int i,j;
     
+    while(scanf("%s",ch)!=EOF){
+        getchar();
+        max = ch[0];
+
+        for(i=0;i<strlen(ch);i++)
+            if(ch[i]>max)
+                max = ch[i];
+        
+        for(i=0;i<strlen(ch);i++)
+            if(ch[i]>=max)
+                printf("%c(max)",ch[i]);
+            else
+                printf("%c",ch[i]);
+
+        printf("\n");
+    }
 
 
     return 0;
