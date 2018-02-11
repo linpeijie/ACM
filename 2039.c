622a28ccc015600b1237624bcf11c3afe0cc10c4
lpj498840744 <lpj498840744@qq.com>
2018-02-11 09:11:43 +0800

    Determine whether the triangle is true

---
+++ 2039.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+/*The relationship between three sides of a triangle:The sum of both sides is greater than the third,and the difference between the two sides is less than the third.*/
+int main(){
+    int m;
+    int a,b,c;
+
+    scanf("%d",&m);
+    while(m--){
+        scanf("%d%d%d",&a,&b,&c);
+
+        if(a+b>c && a+c>b && b+c>a && abs(a-b)<c && abs(a-c)<b && abs(b-c)<a)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
