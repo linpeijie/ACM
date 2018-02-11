e2da2445b9318827f92c335e0e232888deb10941
linpeijie <lpj498840744@qq.com>
2018-02-11 15:48:12 +0800

    whether the password is true

--- 2043.c
+++ 2043.c
@@ -1,60 +1,31 @@
 #include<stdio.h>
 #include<string.h>
 
-int main(){
-    int M,i,YES,length,index;
-    char ch[50];
-    int flag[50];
-
-    scanf("%d",&M);
-
-    while(M--){
-        scanf("%s",ch);
-        getchar();
-
-        length = strlen(ch);
-
-        if(strlen(ch)<8 || strlen(ch)>16){
-            printf("NO\n");
-            continue;
-        }
-
-        for(i=0;i<strlen(ch);i++){
-            if(ch[i]>='A' && ch[i]<='Z')
-                flag[i]=1;
-            else if(ch[i]>='a' && ch[i]<='z')
-                flag[i]=2;
-            else if(ch[i]>='0' && ch[i]<='9')
-                flag[i]=3;
-            else if(ch[i]=='~'||ch[i]=='!'||ch[i]=='@'||ch[i]=='#'||ch[i]=='$'||ch[i]=='%'||ch[i]=='^')
-                flag[i]=4;
-            else
-                flag[i]=0;
-        }
-
-        for(i=0;i<length;i++)
-            if(flag[i]==0)
-                printf("NO\n");
-
-        int j;
-        for(i=0;i<length;i++)
-            for(j=0;j<length-i-1;j++)
-                if(flag[j]>flag[j+1]){
-                   index = flag[j]; 
-                   flag[j] = flag[j+1];
-                   flag[j+1] = index;
-                }
-
-        for(YES=1,i=1;i<length;i++)
-            if(flag[i]!=flag[i-1])
-                YES++;
-
-        if(YES>=3)
+int main()
+{
+	int i,j,k,l,n,m,q;
+	char a[50];
+	char b[7]={'~','!','@','#','$','$','%'};
+	scanf("%d",&n);
+	getchar();
+	while(n--)
+	{
+		j=k=l=q=0;
+		gets(a);
+		m=strlen(a);
+		for(i=0;i<m;i++)
+		{
+			if((a[i]>=65)&&(a[i]<=90))j=1;
+			if((a[i]>=95)&&(a[i]<=122))k=1;
+			if((a[i]>=48)&&(a[i]<=57))l=1;
+		for(int t=0;t<7;t++)
+		{if(a[i]==b[t])q=1;
+		}
+		}
+		if(((j+k+l+q)>=3)&&m>=8&&m<=16)
             printf("YES\n");
-        else
-            printf("NO\n");
-    }
-
-
+		else
+		    printf("NO\n");
+	 }
     return 0;
-}
+ }
