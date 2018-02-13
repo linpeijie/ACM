f482ff8cd28b64c2fa9500f015e63dd4e0679a83
linpeijie <lpj498840744@qq.com>
2018-02-13 21:45:10 +0800

    literal

--- 2048.c
+++ 2048.c
@@ -2,21 +2,21 @@
 /*错排问题，或容错问题*/
 
 int main(){
-    int c,n,i,sum=1;
-    long long result[30]={0,1},A[30];
+    int c,n,i;
+    long long sum=1,result[30]={0,0,1},A[30];
 
     for(i=1;i<30;i++){
         sum *= i;
         A[i] = sum;
     }
 
-    for(i=2;i<30;i++)
+    for(i=3;i<30;i++)
         result[i] = (i-1)*(result[i-1]+result[i-2]);
 
     scanf("%d",&c);
     while(c--){
         scanf("%d",&n);
-        printf("%.2f%%\n",result[n]*100.0/A[n]);
+        printf("%.2llf%%\n",result[n]*100.0/A[n]);
     }
 
     return 0;
