1bf62a2899d1d2fbd8386f1165fd78140209b268
linpeijie <lpj498840744@qq.com>
2018-02-17 21:46:50 +0800

    Exact large Numbers.

--- 2054.c
+++ 2054.c
@@ -1,12 +1,40 @@
 #include<stdio.h>
 #include<string.h>
 
+void process(char a[])
+{
+	int i, j,len,dot=-1;
+
+	len = strlen(a);
+	for (i = 0; i <= len - 1; i++)
+	{
+		if (a[i] == '.')
+		{
+			dot = i;
+			break;
+		}
+	}
+	if (dot >= 0)
+	{
+		for (i = len - 1;i>=0; i--)
+		{
+			if (a[i] != '0')
+				break;
+			else
+				a[i] = '\0';
+		}
+		if (a[i] == '.')
+			a[i] = '\0';
+	}
+}
+
 int main(){
-    char A[1000],B[1000];
+    char A[100000],B[100000];
 
     while(scanf("%s%s",A,B)!=EOF){
-        getchar();
 
+        process(A);
+        process(B);
         if(!strcmp(A,B))
             printf("YES\n");
         else
@@ -16,3 +44,4 @@ int main(){
 
     return 0;
 }
+
