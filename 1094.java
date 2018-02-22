e19ddbc9577ddf803e93f09a65de956e931e9dc3
linpeijie <lpj498840744@qq.com>
2018-02-22 17:13:14 +0800

    I/O practice with Java

--- 1094.java
+++ 1094.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+/*
+注意判断 scanf.hasNext(),相当于C中的 scanf（）!= EOF。
+*/
 
 class Main{
     public static void main(String[] args){
@@ -6,7 +9,7 @@ class Main{
 
         int N,n,sum;
 
-        while(true){
+        while(scanf.hasNext()){
             sum = 0;
             N = scanf.nextInt();
 
