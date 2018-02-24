c85ea428becae33c50610fad1ca7f5cf9aac051b
lpj498840744 <lpj498840744@qq.com>
2018-02-24 19:55:40 +0800

    Large numbers of the sum problem

--- 2058.c
+++ 2058.c
@@ -4,15 +4,13 @@ int main(){
     long long N,M,sum,i,j,length;
 
     while(scanf("%lld%lld",&N,&M)!=EOF && (N!=0&&M!=0)){
-        length = M/2;
+        length = M/3;
 
-        for(i=1;i<=M/2;i++){
+        for(i=1;i<=M/3;i++){
             sum = 0;
             
             for(j=i;j<=i+length;j++){
                 sum +=j;
-                if(sum>M)
-                    break;
 
                 if(sum == M){
                     length = j-i+1;
