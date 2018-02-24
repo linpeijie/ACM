2af59592282201ab4140a58a47dff1c01cb71d18
lpj498840744 <lpj498840744@qq.com>
2018-02-24 20:38:03 +0800

    DP

--- 2058.c
+++ 2058.c
@@ -4,13 +4,16 @@ int main(){
     long long N,M,sum,i,j,length;
 
     while(scanf("%lld%lld",&N,&M)!=EOF && (N!=0&&M!=0)){
-        length = M/3;
+        length = M/2;
 
         for(i=1;i<=M/3;i++){
             sum = 0;
             
             for(j=i;j<=i+length;j++){
                 sum +=j;
+
+                if(sum > M)
+                    break;                
 
                 if(sum == M){
                     length = j-i+1;
