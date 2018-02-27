d37da23fa46fbd777fda6aeb2cf5d9b2d94938cd
lpj498840744 <lpj498840744@qq.com>
2018-02-27 20:33:10 +0800

    printf char

--- 2091.c
+++ 2091.c
@@ -5,23 +5,25 @@ int main(){
     char ch;
 
     while(scanf("%c%d",&ch,&n) && ch!='@'){
+
         for(i=1;i<n;i++){
             count = 2*n-1;
-            for(j=1;j<=count/2;j++){
-                if(j!=count/2-i+1)
+            for(j=1;j<=count/2+1;j++){
+                if(j!=count/2+2-i)
                     printf(" ");
                 else
                     printf("%c",ch);
             }
 
-            for(j=count/2+1;j<=count/2+i;j++){
-                if(i==1)
-                    printf("\n");
-                else if(j==count/2+i && i!=1)
-                    printf("%c\n",ch);
-                else
-                    printf(" ");
+            for(j=count/2;j<count;j++){
+                if(i!=1){
+                    if(j==count/2+i-2)
+                        printf("%c",ch);
+                    else
+                        printf(" ");
+                }                    
             }
+            printf("\n"); 
         }
 
         for(i=1;i<=2*n-1;i++){
@@ -30,6 +32,7 @@ int main(){
             else
                 printf("%c\n",ch);
         }
-     }
+        printf("\n");
+    } 
     return 0;
 }
