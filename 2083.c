620a5dfc0c6b50bfe40260753ef795a66a2b579c
linpeijie <lpj498840744@qq.com>
2018-02-28 16:45:04 +0800

    A+B problem

--- 2083.c
+++ 2083.c
@@ -13,11 +13,11 @@ int main(){
 
         min = 0;
         time = 0;
-        for(i=0;i<N;i++)
-            for(j=0;j<N;j++)
-                min += abs(x[j]-x[i]);
+        for(j=0,i=0;j<N;j++)
+            min += abs(x[j]-x[i]);
         
         for(i=1;i<N;i++){
+            time = 0;
             for(j=0;j<N;j++)
                 time += abs(x[j]-x[i]);
             if(time < min)
