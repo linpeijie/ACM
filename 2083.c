6673d7b2d3bd2782ea80f68f1c029c59d15bf412
linpeijie <lpj498840744@qq.com>
2018-02-28 16:27:22 +0800

    A+B problem

---
+++ 2083.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int j,i,M,N,x[501];
+    int min,time;
+
+    scanf("%d",&M);
+    while(M--){
+        scanf("%d",&N);
+        for(i=0;i<N;i++)
+            scanf("%d",&x[i]);
+
+        min = 0;
+        time = 0;
+        for(i=0;i<N;i++)
+            for(j=0;j<N;j++)
+                min += abs(x[j]-x[i]);
+        
+        for(i=1;i<N;i++){
+            for(j=0;j<N;j++)
+                time += abs(x[j]-x[i]);
+            if(time < min)
+                min = time;
+        }
+        printf("%d\n",min);
+    }
+    return 0;
+}
