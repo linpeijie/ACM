40cc567ff6411b970cb8d33de5f3af04a34c7be4
linpeijie <lpj498840744@qq.com>
2018-03-01 20:51:09 +0800

    Spring problem

--- 2072.c
+++ 2072.c
@@ -7,36 +7,44 @@ typedef struct {
 
 int main(){
     char sentence[2000],letter[100];
-    int count,i,j,flag;
+    int count,i,j,l,flag;
     memory m[1000];    
 
     while(gets(sentence) && strcmp(sentence,"#")){
-        j=0;
+        l=0;
         count = 0;
 
-        for(i=0;i<strlen(sentence);i++){
+        for(i=0;i<=strlen(sentence);i++){
             flag = 1;
-            
-            if(sentence[i]!=' ')
-                letter[j++] = sentence[i];
+             
+            if(sentence[i]!=' ' && (i!=strlen(sentence)))
+                letter[l++] = sentence[i];
             else{
-                j=0;
-                memset(letter,0,sizeof(letter));
+                if(l==0)
+                    continue;
+
+                l=0;
                 
-                for(j=0;j<count;j++)
-                    if(strcmp(m[j].ch,letter)){
+                for(j=0;j<count;j++){
+                    if(strcmp(m[j].ch,letter))
+                        flag = 1;
+                    else{
                         flag = 0;
                         break;
                     }
+                }
 
                 if(flag){
-                    strcpy(m[count++].ch,letter);
+                    strcpy(m[count].ch,letter);
+                    count++;
                 }
+
+                memset(letter,'\0',sizeof(letter));
             }
 
         }
         
-        printf("%d\n",count+1);
+        printf("%d\n",count);
     }
     return 0;
 }
