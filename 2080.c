66cefb3444186925cbb1d63f095b556e6b6b69ba
linpeijie <lpj498840744@qq.com>
2018-03-01 13:31:05 +0800

    Calculate the angle

--- 2080.c
+++ 2080.c
@@ -10,11 +10,11 @@ int main(){
     
     while(T--){
         scanf("%lf%lf%lf%lf",&x1,&y1,&x2,&y2);
-
-        angle1 = atan2(y1,x1)*180/PI;
+        
+        angle1 = atan2(y1,x1)*180/PI;                    //计算两点各自的与X轴正方向的夹角
         angle2 = atan2(y2,x2)*180/PI;
 
-        if((y1>=0 && y2>=0)||(y1<=0 && y2<=0))
+        if((y1>=0 && y2>=0)||(y1<=0 && y2<=0))           //计算两条线段之间的夹角
             printf("%.2lf\n",fabs(angle1-angle2));
         else{
             if(angle1+angle2 <= 180)
