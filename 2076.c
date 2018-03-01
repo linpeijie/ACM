59e8e3c9d52ace14633cff8a2ac7ea1fd182f20b
linpeijie <lpj498840744@qq.com>
2018-03-01 14:45:45 +0800

    Calculate the angle

--- 2076.c
+++ 2076.c
@@ -10,16 +10,14 @@ int main(){
         while(T--){
         scanf("%d%d%d",&h,&m,&s);
 
-        h>=12?(h=h-12):h;
+        h>=12?(h=h-12):h;                //换成12小时制
 
-        M = 6*(m+1.0*s/60);
+        M = 6*(m+1.0*s/60);              //分别计算时针和分针与12原点的夹角
         H = 30*(h+(1.0*s/60+m)/60);
 
-        angle = H-M;
+        angle = fabs(H-M);
             
-        if(angle<0)
-            angle=-angle;
-        if(angle>180)
+        if(angle>180)                     //选用合格的夹角
             angle = 360-angle;
             
         printf("%d\n",(int)angle);
