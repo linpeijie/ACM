5232439effa282d38fbb202b2248b29950cbc61c
linpeijie <lpj498840744@qq.com>
2018-03-01 14:20:03 +0800

    Calculate the angle

---
+++ 2076.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int T;
+    int h,m,s;
+    int H,M,S,angle;
+
+    scanf("%d",&T);
+
+    while(T--){
+        scanf("%d%d%d",&h,&m,&s);
+
+        M = 6*m+6.0/60*s;
+        H = 30*(h+(1.0*s/60+m)/60);
+
+        angle = (abs(H-M)>180)?360-abs(H-M):abs(H-M);
+
+        printf("%d\n",angle);
+    }
+    return 0;
+}
