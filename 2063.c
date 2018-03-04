ba4577a21e1a961ef640f88697032963e4e96fa3
linpeijie <lpj498840744@qq.com>
2018-03-04 18:54:03 +0800

    hungray algorithm

--- 2063.c
+++ 2063.c
@@ -1,65 +1,42 @@
 #include<stdio.h>
 #include<string.h>
 
-int swap(int a[], int b[]){
-    int c[1000];
-    int i;
-    
-    for(i=0;i<sizeof(b);i++)
-        a[i] = b[i];
+int M,N,people[505][505],m[505],boy[505];
 
+int find(int n){
+    int k;
+    for(k = 1; k <= N; k++) {
+        if(people[n][k] == 1 && m[k] != 1) {
+            m[k] = 1;
+            if(boy[k] == 0 || find(boy[k])) {
+                boy[k] = n;
+                return 1;
+            }
+        }
+    }
     return 0;
 }
 
 int main(){
-    int K,M,N,i,j,l,A,B,count,max;
-    int people[1000][1000],m[1000];
+    int K,i,A,B,count;
 
     while(scanf("%d",&K),K) {
         scanf("%d%d",&M,&N);
-        memset(people,'\0',sizeof(people));
+        memset(people,0,sizeof(people));
+        memset(boy,0,sizeof(boy));
+        count = 0;
+
         while(K--) {
             scanf("%d%d",&A,&B);
             people[A][B] = 1;
-            people[A][0]++;
         }
         
-        for(i = 1; i <= M; i++) {
-            for(j = 1; j <= M-i; j++) {
-                if(people[j][0] < people[j+1][0]){
-                    swap(m,people[j]);
-                    swap(people[j],people[j+1]);
-                    swap(people[j+1],m);
-                }    
-            }
-        }
-
-        memset(m,'\0',sizeof(m));
-        
-        count = max = 0;
-        for(l = 1; l <= N; l++) {
+        for(i = 1; i <= M; i++){
             memset(m,0,sizeof(m));
-            
-            if(people[1][l] == 1) {
-                m[l] = 1;
-                count = 1;
-                for(i = 2; i <= M; i++)
-                    for(j = 1; j <= N; j++) {
-                        if(people[i][j] == 1) {
-                            if(m[j] != 1) {
-                                m[j] = 1;
-                                count++;
-                                break;
-                            }
-                        }
-                    }
-            } else continue;
-
-            if(count > max)
-                max = count;
+            if(find(i))
+                count++;
         }
-
-        printf("%d\n",max);
+        printf("%d\n",count);
     }
     return 0;
 }
