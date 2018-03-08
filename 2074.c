07ce0139ce77d699bd71dde035df06f8aedba249
linpeijie <lpj498840744@qq.com>
2018-03-08 14:07:13 +0800

    print char

--- 2074.c
+++ 2074.c
@@ -1,35 +1,48 @@
 #include<stdio.h>
+#include<string.h>
 
 int main(){
     int n,i,j;
     int flag;
-    char c,e;
+    char c,e,ch;
+    char a[81][81];
 
     flag = 0;
     while(scanf("%d %c %c",&n,&c,&e)!=EOF){
         getchar();
-
+        
+        memset(a,'\0',sizeof(a));
         if(flag)
             printf("\n");
 
-        for(i=0;i<n;i++){
-            if(i == 0 || i == n-1)
-                printf(" ");
-            else
-                printf("%c",e);
+        for(i = 1; i <= (n/2)+1; i++){
+             for(j = 0; j < n; j++){
+                 if((j < i || j > n-i-1 ) && j%2 == 0){
+                     if(i == 1 && (j == 0 || j == n-1)){
+                         printf(" ");
+                         ch = e;
+                         a[i][j] = ' ';
+                         continue;
+                     }
+                     printf("%c",e);
+                     ch = e;
+                     a[i][j] = e; 
+                 }
+                 else if((j < i || j > n-i) && j%2 == 1){
+                     printf("%c",c);
+                     ch = c;
+                     a[i][j] = c;
+                 }
+                 else{
+                     printf("%c",ch);
+                     a[i][j] = ch;
+                 }
+             }
+             printf("\n");
         }
 
-        for(i = 1; i < n-1; i++){
-            printf(" \n"); 
-
-        }
-
-        for(i = 0; i < n; i++){
-            if(i == 0 || i == n-1)
-                printf(" ");
-            else
-                printf("%c",e);
-        }
+        for(i = n/2; i > 0; i--)
+            printf("%s\n",a[i]);
 
         flag = 1;
     }
