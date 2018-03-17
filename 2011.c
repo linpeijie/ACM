14fd90612de781d1025851293c16f0e15877a41e
linpeijie <lpj498840744@qq.com>
2018-03-17 23:16:48 +0800

    计算数列前n项和

--- 2011.c
+++ 2011.c
@@ -1,28 +1,29 @@
+/* 记忆化搜索，打表，也是动态规划的一个重要思想
+*/
+
 #include<stdio.h>
 
 int main(){
     int n[1000];
     int m,i,j;
-    double result;
+    double result[1001];
 
+    result[1]=1;
+    for(j = 2; j <= 1000; j++){
+            if(j % 2 != 0)
+                result[j] = result[j-1] + 1.0/j;
+            else
+                result[j] = result[j-1] - 1.0/j;            
+    }
+    
     scanf("%d",&m);
-    for(i=0;i<m;i++){
+    for(i = 0;i < m; i++){
         scanf("%d",&j);
         n[i] = j;
     }
 
-    for(i=0;i<m;i++){
-
-        result=0;
-
-        for(j=1;j<=n[i];j++){
-            if(j%2!=0)
-                result += 1.0/j;
-            else
-                result -= 1.0/j;            
-        }
-
-        printf("%.2lf\n",result);
+    for(i=0;i<m;i++){ 
+        printf("%.2lf\n",result[n[i]]);
     }
     
     return 0;
