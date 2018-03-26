c460a5802bbb802c548866cb320024aff087f9ae
linpeijie <lpj498840744@qq.com>
2018-03-26 19:20:20 +0800

    prime

--- 2098.c
+++ 2098.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<string.h>
 
 bool isPrimeNumber(int n){
     int i;
@@ -14,13 +15,23 @@ bool isPrimeNumber(int n){
 }
 
 int main(){
-    int n,i,count;
+    int n,i,j,count;
+    int prime[10005];
+    
+    memset(prime,1,sizeof(prime));
+    prime[0] = 0;
+    prime[1] = 0;
+
+    for(i = 2; i < 10005; i++)
+        if(prime[i])
+            for(j = 2*i; j < 10005; j += i)
+                prime[j] = 0;
 
     while(scanf("%d",&n)!=EOF,n){
         count = 0;
 
         for(i=1;i<n/2;i+=2){
-            if(isPrimeNumber(i)&&isPrimeNumber(n-i))
+            if(prime[i] && prime[n-i])
                 count++;
         }
 
