9e9d64248c39920d99eb596320ce667a84cfe8d4
linpeijie <lpj498840744@qq.com>
2018-03-26 19:22:13 +0800

    prime

--- 2098.c
+++ 2098.c
@@ -1,18 +1,5 @@
 #include<stdio.h>
-#include<stdbool.h>
 #include<string.h>
-
-bool isPrimeNumber(int n){
-    int i;
-    if(n==0 || n==1)
-        return false;
-
-    for(i=2;i<n/2;i++)
-        if(n%i==0)
-            return false;
-    
-    return true;
-}
 
 int main(){
     int n,i,j,count;
